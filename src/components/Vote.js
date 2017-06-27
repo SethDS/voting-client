a8a10524735d87c4e2117cf7737457ef7577994a
Seth Sutherland <cokolatepal@gmail.com>
2017-06-27 16:31:50 -0600

    working on vote component

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -8,6 +8,17 @@ export default React.createClass({
    getPair: function(){
        return this.props.pair || [];
    },
-    
+    isDisabled: function(){
+       return !!this.props.hasVoted;
+    },
+    hasVotedFor: function(entry){
+        return this.props.hasVoted === entry;
+    },
+    render: function(){
+        return <div className="voting">
+            
+        </div>
+    }
+
 
 });
